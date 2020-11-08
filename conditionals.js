786424cd7fdaab355bb7c50fcccb6547e2102a02
mallory-erik <malloryerik@gmail.com>
2020-11-09 00:49:26 +0900

    ...

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+
+const mygrade = 92
+
+function grader(grade) {
+  switch (grade) {
+    case (grage == 100):
+      console.log("Perfect, and...")
+    case (grade > 95):
+      console.log("Awesome")
+      break
+    case (grade > 89):
+      console.log("Nice work")
+      break
+    case (grade > 82):
+      console.log("Not bad")
+      break
+    case (grade > 75):
+      console.log("Sketchy")
+    case (grade > 65):
+      console.log("Bad news")
+    default:
+      console.log("Fail")
+      break
+  }
+}
+
